4a03a732393925abcedbf7a6c0995ee424665274
Amirhossein Razavi <108837052+razaviah@users.noreply.github.com>
2022-09-23 23:33:26 -0400

    Update shell_script.sh

--- scripts/shell_script.sh
+++ scripts/shell_script.sh
@@ -39,6 +39,7 @@ exec > >(tee ${LOG_FILE}) 2>&1
 # PART 5: DOWNLOAD DATA
 echo "Start download data"
 
+# We only need the data of Station ID = 48549.
 for year in {2020..2022}; # or use (seq 2019 2022)
 do wget -N --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=48549&Year=${year}&Month=2&Day=14&timeframe=1&submit= Download+Data" -O ${INPUT_FOLDER}/${year}.csv;
 done;
